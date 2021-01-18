8a1696ba252e266c0c8fcf90d13024f4c3bd9d7f
JohnDoubleUB <up741132@myport.ac.uk>
2021-01-18 15:29:57 +0000

    change

--- Assets/Scripts/LadderScript.cs
+++ Assets/Scripts/LadderScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LadderScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    //Added a comment to test
+    //Added a comment to tests
     void Start()
     {
         
