840e4ecb63873a0b953f0d1ab6e988dcff9b11b9
JohnDoubleUB <up741132@myport.ac.uk>
2021-01-19 20:43:23 +0000

    Added main menu of sorts, did intial setup of pause menu but no code yet

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager current;
+
+    private void Awake()
+    {
+        if (current != null) Debug.LogWarning("Oops! it looks like there might already be a GameManager in this scene!");
+        current = this;
+    }
+}
