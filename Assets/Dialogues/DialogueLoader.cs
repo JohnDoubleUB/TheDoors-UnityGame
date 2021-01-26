fcdee42b951cf61eddd686b6bd55013973caa131
JohnDoubleUB <up741132@myport.ac.uk>
2021-01-26 01:23:00 +0000

    Created method for reading xml dialogue and building a dialogue tree. Needs to be tested and refined

--- Assets/Dialogues/DialogueLoader.cs
+++ Assets/Dialogues/DialogueLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using UnityEngine;
+using System.Linq;
 
 public static class DialogueLoader
 {
@@ -20,7 +21,7 @@ public static class DialogueLoader
     //Attributes: id, tree-name, dialogue-id
 
 
-    public static void LoadDialogueFile(string path) 
+    public static void LoadDialogueFile(string path) //TODO: This should return something!
     {
 
         if (File.Exists(path))
@@ -28,19 +29,16 @@ public static class DialogueLoader
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
 
-            string attr;
-
             //Find top level xml node!
             XmlNode topLevelNode = doc.DocumentElement.SelectSingleNode("/dialogue-object");
 
-            XmlNodeList xmlNodes = topLevelNode.SelectNodes("dialoguetree");
+            XmlNodeList xmlNodes = topLevelNode.SelectNodes("dialogue-tree");
 
             Debug.Log("Node count" + xmlNodes.Count);
 
             foreach (XmlNode xmln in xmlNodes) 
             {
-                attr = xmln.Attributes["tree-name"]?.InnerText;
-                if (!string.IsNullOrEmpty(attr)) Debug.Log(attr);
+                Debug.Log("Node name?: " + xmln.Name);
             }
 
             Debug.Log("Successfully opened");
@@ -51,9 +49,80 @@ public static class DialogueLoader
         }
   
     }
+
+    //TODO: run various data through this to make sure it works properly, and genrally cleanup this mess! I haven't run this yet but I think it mostly should work
+    private static DialogueTree BuildDialogueTree(XmlNode dialogueTree) 
+    {
+        List<Dialogue> dList = new List<Dialogue>();
+        //First loop
+
+        XmlNodeList xmlDialogues = dialogueTree.SelectNodes("Dialogue");
+
+        foreach (XmlNode xmlDialogue in xmlDialogues) 
+        {
+            List<XmlNode> xmlDialogueList = new List<XmlNode>(xmlDialogue.ChildNodes.Cast<XmlNode>());
+            
+            string id = xmlDialogue.Attributes["id"].InnerText;
+            
+            bool endsConversation = xmlDialogueList.Any(x => x.Name == "speaker-end");
+
+            string[] speaker = xmlDialogueList
+                .Where(x => x.Name == "speaker" || x.Name == "speaker-end")
+                .Select(x => x.InnerText)
+                .ToArray();
+
+            DialogueOption[] dialogueOptions = xmlDialogueList
+                .Where(x => x.Name == "option" || x.Name == "option-end")
+                .Select(x => new DialogueOption(x.InnerText, x.Name == "option-end", x.Attributes["dialogue-id"].InnerText))
+                .ToArray();
+
+            dList.Add(new Dialogue(id, speaker, endsConversation, dialogueOptions));
+        }
+
+        return new DialogueTree(dialogueTree.Attributes["id"].InnerText, dialogueTree.Attributes["name"].InnerText, dList.ToArray());
+    }
 }
 
-public class dialogueSequence 
+public class DialogueTree
 {
+    public string Id;
+    public string Name;
+    Dictionary<string, Dialogue> Dialogues;
 
+    public DialogueTree(string id, string name, Dialogue[] dialogues) 
+    {
+        Id = id;
+        Name = name;
+        if(dialogues != null && dialogues.Length > 0) Dialogues = dialogues.ToDictionary(x => x.Id, x => x);
+    }
+}
+
+public class Dialogue 
+{
+    public string Id;
+    public string[] Speaker;
+    public bool EndsConversation;
+    public DialogueOption[] DialogueOptions;
+
+    public Dialogue(string id, string[] speaker, bool endsConversation, DialogueOption[] dialogueOptions) 
+    {
+        Id = id;
+        Speaker = speaker;
+        EndsConversation = endsConversation;
+        DialogueOptions = dialogueOptions;
+    }
+}
+
+public class DialogueOption 
+{
+    public string OptionText;
+    public bool EndsConversation;
+    public string DialogueID;
+
+    public DialogueOption(string optionText, bool endsConversation, string dialogueId) 
+    {
+        OptionText = optionText;
+        EndsConversation = endsConversation;
+        DialogueID = dialogueId;
+    }
 }
