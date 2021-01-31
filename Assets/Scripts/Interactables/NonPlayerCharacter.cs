5f28efa034fc35dad092f23ee81d9fdb3b4f0c39
JohnDoubleUB <up741132@myport.ac.uk>
2021-01-31 05:50:24 +0000

    Created simple npc script and prefab for interactions

---
+++ Assets/Scripts/Interactables/NonPlayerCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayerCharacter : Interactable
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+
+    }
+
+    new void Awake() 
+    {
+        base.Awake();
+
+    }
+    public override void Interact() 
+    {
+        Debug.Log("npc interact!");
+    }
+}
