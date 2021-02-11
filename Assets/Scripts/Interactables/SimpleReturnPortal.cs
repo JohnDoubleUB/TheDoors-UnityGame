4ad22aed17f3681ae2d53b02d912359714096413
JohnDoubleUB <up741132@myport.ac.uk>
2021-02-11 21:27:07 +0000

    Setup DoorRootings that should allow us to easily set what levels doors will lead to. Also created a simple return portal that we can use to return to the hubworld

---
+++ Assets/Scripts/Interactables/SimpleReturnPortal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleReturnPortal : Interactable
+{
+    public string sceneToReturnTo;
+
+    public override void Interact()
+    {
+        GameManager.current.ChangeLevel(sceneToReturnTo);
+    }
+}
