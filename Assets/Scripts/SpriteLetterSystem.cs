1a2968bdf2666a7884f18b0a0a70c7a5da29c244
JohnDoubleUB <up741132@myport.ac.uk>
2021-02-14 18:29:31 +0000

    More neatness things

--- Assets/Scripts/SpriteLetterSystem.cs
+++ Assets/Scripts/SpriteLetterSystem.cs
@@ -7,25 +7,24 @@ using UnityEngine;
 public class SpriteLetterSystem : MonoBehaviour
 {
     public Texture2D charSheet;
-
-    /* 
-     * Notes for future reference:
-     * Texture2D objects .GetPixel() is janky as hell and can cause unity to crash or freeze (its great) so be aware.
-     * If you're image is larger than its set MaxSize in the inspector then set it up so that this doesn't change the size of the loaded image, if you don't you'll have a bad time
-     * Resources.LoadAll<>() it loads assets from a Resources folder, this can be in various places but its probably best to put it in your Assets folder
-     * Don't be tempted to use AssetDatabase because that only works in the editor and if your game compiled requires this you'll be in trouble
-     * Check your image is loaded at the right resolution properly first, I didn't and it was the last thing I ended up checking.
-     */
-
+    public Dictionary<char, CharData> loadedFont;
     private void Awake()
     {
-        Dictionary<char, CharData> testThing = FontLoader.LoadFontResource(charSheet);
+        loadedFont = FontLoader.LoadFontResource(charSheet);
 
         //https://youtu.be/jhwfA-QF54M?t=155 I wish this video was actually a tutorial
         //This is for testing that the characters are saved properly
+        
+        
+        
+        
+        
+        
+        
+        
         List<string> charAndLength = new List<string>();
 
-        foreach (KeyValuePair<char, CharData> cD in testThing)
+        foreach (KeyValuePair<char, CharData> cD in loadedFont)
         {
             //Debug.Log("Char: " + cD.Key + ", CharWidth: " + cD.Value.Width);
             charAndLength.Add("[" + cD.Key + cD.Value.Width + "]");
@@ -33,4 +32,10 @@ public class SpriteLetterSystem : MonoBehaviour
 
         Debug.Log("Letter info: " + string.Join(", ", charAndLength));
     }
+
+
+
+
+
+
 }
